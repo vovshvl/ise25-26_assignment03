f1bd754bcde3684fbb58a7ba00f4c37ebc2706d6
Kirill <knightromanov@gmail.com>
2025-11-05 20:40:11 +0100

    Fixed test (senior level fix)

--- application/src/test/java/de/seuhd/campuscoffee/systest/PosSystemTests.java
+++ application/src/test/java/de/seuhd/campuscoffee/systest/PosSystemTests.java
@@ -72,8 +72,6 @@ public class PosSystemTests extends AbstractSysTest {
         // Verify changes persist
         Pos retrievedPos = posDtoMapper.toDomain(TestUtils.retrievePosById(posToUpdate.getId()));
 
-        // Break test case
-        posToUpdate.setName("THIS STRING BREAKS THE TEST");
 
         assertThat(retrievedPos)
                 .usingRecursiveComparison()
